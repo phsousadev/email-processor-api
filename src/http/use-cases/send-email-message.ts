25d73edac7d3227c7db11864a596b9490a3552f2
Pedro Sousa <pedro.sousa@divam.com.br>
2025-06-01 12:34:53 -0300

    feat: implementing use case for sending emails

---
+++ src/http/use-cases/send-email-message.ts
@@ -0,0 +1,24 @@
+import { EmailMessageRepository } from '@/repositories/email-message-repository'
+
+interface ISendEmailMessageRequest {
+  to: string
+  subject: string
+  body: string
+}
+
+export class SendEmailMessageUseCase {
+  constructor(private emailMessageRepository: EmailMessageRepository) {}
+
+  async execute({ to, subject, body }: ISendEmailMessageRequest) {
+    const email = await this.emailMessageRepository.create({
+      to,
+      subject,
+      body,
+      status: 'SENT',
+    })
+
+    return {
+      email,
+    }
+  }
+}
