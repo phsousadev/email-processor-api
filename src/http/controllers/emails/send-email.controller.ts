5ea1d4e915071b47f652587aa49be783950da7e7
Pedro Sousa <pedro.sousa@divam.com.br>
2025-06-01 13:04:13 -0300

    feat: implementing controller for sending email

---
+++ src/http/controllers/emails/send-email.controller.ts
@@ -0,0 +1,34 @@
+import { EmailMessageFactoryUseCase } from '@/http/use-cases/factories/email-message-factory'
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+export async function sendEmail(request: FastifyRequest, reply: FastifyReply) {
+  const sendEmailBodySchema = z.object({
+    to: z
+      .string()
+      .email({ message: 'Destinatário deve ser um e-mail válido' })
+      .max(320, { message: 'E-mail muito longo' }),
+
+    subject: z
+      .string()
+      .min(1, { message: 'Assunto é obrigatório' })
+      .max(255, { message: 'Assunto deve ter no máximo 255 caracteres' }),
+
+    body: z
+      .string()
+      .min(1, { message: 'Corpo da mensagem é obrigatório' })
+      .max(5000, { message: 'Corpo da mensagem muito longo' }),
+  })
+
+  const { to, subject, body } = sendEmailBodySchema.parse(request.body)
+
+  const sendEmailUseCase = EmailMessageFactoryUseCase()
+
+  await sendEmailUseCase.execute({
+    to,
+    subject,
+    body,
+  })
+
+  return reply.status(201).send()
+}
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,7 +1,11 @@
 import { FastifyInstance } from 'fastify'
+import { sendEmail } from './controllers/emails/send-email.controller'
 
 export async function routes(app: FastifyInstance) {
   app.get('/', async (request, reply) => {
     reply.send({ message: `Hello World` })
   })
+
+  // Emails
+  app.post('/email', sendEmail)
 }
