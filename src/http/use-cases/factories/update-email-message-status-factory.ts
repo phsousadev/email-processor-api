52069de4c2e53ea9c526c2ccf303db8b5604ff2a
Pedro Sousa <pedro.sousa@divam.com.br>
2025-06-02 17:44:11 -0300

    chore: correcting nomenclature

--- src/http/use-cases/factories/update-email-message-status-factory.ts
+++ src/http/use-cases/factories/update-email-message-status-factory.ts
@@ -1,7 +1,7 @@
 import { PrismaEmailMessageRepository } from '@/repositories/prisma/prisma-email-message-repository'
 import { UpdateEmailMessageStatusUseCase } from '../update-email-message-status'
 
-export function UpdateEmailMessageFactoryUseCase() {
+export function UpdateEmailMessageStatusFactoryUseCase() {
   const prismaEmailMessageRepository = new PrismaEmailMessageRepository()
   const sendEmailMessageUseCase = new UpdateEmailMessageStatusUseCase(
     prismaEmailMessageRepository,
